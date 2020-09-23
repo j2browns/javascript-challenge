34f3cef69731230a28c85b541585cc88dc646493
unknown <j2browns@yahoo.com>
2020-09-22 23:12:04 -0500

    code accepts date field and puts table on page

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,37 +1,61 @@
 // Get a reference to the table body
-var tbody = d3.select("tbody");
+//var tbody = d3.select("tbody");
 
 // from data.js
 var tableData = data;
-    console.log("is this working");
-  //  console.log(tableData[0]);
-    console.log(Object.keys(tableData[0]));
- //testing using forEach loop to make certain have data transferring and can filter - show on console  
-    // tableData.forEach(function(object) {
-    //     test = object.state
-    //     if (test.toLowerCase() === "nm") {
-    //         console.log(object);
-    //     };
-    // });
+    console.log("is this working");//verify connection to html code
+  
+var inputDate = d3.select("#form"); //check form
+inputDate.on("submit",dateFilter); //if form submitted then go to dateFilter
 
-//testing using filter function
-    filterData = tableData.filter(object => object.state === "ar");
-    console.log(filterData);
-// getting unique listing of values
+function dateFilter() {
+  d3.event.preventDefault();
+  console.log("went to function");
+  var inputElement = d3.select("#datetime");
+  var inputValue = inputElement.property("value");
+  console.log(inputValue);
 
-const uniqueDate = [... new Set(tableData.map(object => object.datetime))]
-console.log(uniqueDate)
-
-//writing entire dataset to table
-tableData.forEach((object) => {
+  //filter data based on input field
+  filterData = tableData.filter(object => object.datetime === inputValue);
+  console.log(filterData); //show in console filtered data
+  
+  //filling in table
+  var tbody = d3.select("tbody");
+  filterData.forEach((object) => {
     var row = tbody.append("tr");
     Object.entries(object).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
   });
+};
 
-  var inputDate = d3.select(".form-control").text();
-  
-  
-  console.log(`Date says ${inputDate}`);
+
+// const uniqueDate = [... new Set(tableData.map(object => object.datetime))]
+// console.log(uniqueDate)
+
+
+// var inputDate = d3.select("#form");
+
+// inputDate.on("submit",dateFilter);
+// console.log(`Date says ${inputDate}`);
+
+// var inputElement = d3.select("#datetime");
+
+
+
+
+
+//testing using filter function
+    // filterData = tableData.filter(object => object.state === "ar");
+    // console.log(filterData);
+// getting unique listing of values
+
+//writing entire dataset to table
+// tableData.forEach((object) => {
+//     var row = tbody.append("tr");
+//     Object.entries(object).forEach(([key, value]) => {
+//       var cell = row.append("td");
+//       cell.text(value);
+//     });
+//   });
