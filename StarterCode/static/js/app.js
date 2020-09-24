6d46ab8922f00ac4aeb16f748f3b5e58d626406b
unknown <j2browns@yahoo.com>
2020-09-23 22:26:34 -0500

    clearing table, putting header back, and filtering date and city

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -23,38 +23,76 @@ console.log('Your table has ' + iRowCount + ' rows.');
 //var inputDate = d3.select("#form"); //check form
 //inputDate.on("submit",dateFilter); //if form submitted then go to dateFilter
 
-
 var button = d3.select("#filter-btn");//check button
 button.on("click", dateFilter);
 
+//Function below finds lenght of table on web page
 function tableLength() {
   var oRows = document.getElementById('ufo-table').getElementsByTagName('tr');
   var iRowCount = oRows.length;
   return iRowCount;
 };
 
+
 function dateFilter() {
   d3.event.preventDefault();
   
+  //finding initial length of table and sending to console for trouble shooting
   rowCount = tableLength();
   console.log(`Table Length at start: ${rowCount}`);
+  //clearing any contents that already exist so don't add to existing table
   for (i=0; i<rowCount; i++) {
-    document.getElementById('ufo-table').deleteRow(0);
+    document.getElementById('ufo-table').deleteRow(0);//delete first row and repeat for number of rows in table
   };
+  //finding table length after clearing - should be 0 and show in console
   rowCount = tableLength();
   console.log(`Table Length after clean: ${rowCount}`);
   
-  console.log("went to function");
+  //Getting date and putting on console
   var inputElement = d3.select("#datetime");
-  var inputValue = inputElement.property("value");
-  console.log(inputValue);
+  var inputDate = inputElement.property("value");
+  console.log(inputDate);
 
-  //filter data based on input field
-  filterData = tableData.filter(object => object.datetime === inputValue);
-  console.log(filterData); //show in console filtered data
+  var inputElement = d3.select("#city");
+  var inputCity = inputElement.property("value");
+  console.log(inputCity);
+
+  var inputElement = d3.select("#state");
+  var inputState = inputElement.property("value");
+  console.log(inputState);
+
+
+  //filter data based on date field
+  if (inputDate != "") {
+    filterData = tableData.filter(object => object.datetime === inputDate);
+    console.log(filterData); //show in console filtered data
+  }
+  else {
+    filterData = tableData;
+  };
+  if (inputCity != "") {
+    filterData = filterData.filter(object => object.city === inputCity);
+    console.log(filterData); //show in console filtered data
+  };
+
+  
+  // filterData = tableData.filter(object => object.datetime === inputDate);
+  // console.log(filterData); //show in console filtered data
   
   //filling in table
+
+  var tableHeading = ["Date", "City", "State", "Country", "Shape", "Duration", "Comments"];
+
+  var thead = d3.select("thead");
+  var row = thead.append("tr");
+  for (i = 0; i<tableHeading.length; i++) {
+    var cell = row.append("th");
+    cell.text(tableHeading[i]);
+
+  };
+
   var tbody = d3.select("tbody");
+    
   filterData.forEach((object) => {
     var row = tbody.append("tr");
     Object.entries(object).forEach(([key, value]) => {
